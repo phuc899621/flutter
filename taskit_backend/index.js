3380eb3452097cde3321246f18e72e274cb3ac4a
phuc899621 <phuc899621@gmail.com>
2025-03-26 16:22:04 +0700

    26/3/2025

--- taskit_backend/index.js
+++ taskit_backend/index.js
@@ -4,9 +4,9 @@ const port=8080;
 const UserModel=require('./model/user.model')
 require('dotenv').config();
 
-app.get('/',(req,res)=>{
-    res.send("Hello ds world");
-})
+app.get('/', (req, res) => {
+    res.send('<h1 style="color: blue; font-family: Arial;">Hello Longs from Express!</h1>');
+});
 app.listen(port,()=>{
     console.log(`Server listening on port http://localhost:${port}`);
 });
