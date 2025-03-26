ce38b45c9afaf2a5f960a7ea0b1eee407cd203be
phuc899621 <phuc899621@gmail.com>
2025-03-26 13:57:04 +0700

    phuc_backend 26/3/2025

--- taskit_backend/model/user.model.js
+++ taskit_backend/model/user.model.js
@@ -1,44 +1,43 @@
-const mongoose=require('mongoose');
-const db=require('../config/db');
-const bcrypt=require('bcrypt');
-const { Schema }=mongoose;
-
-const userScheme=new Schema({
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    name:{
-        type: String,
-        required: true
-    },
-    password:{
-        type: String,
-        required: true
-    },
-    avatar: {
-        type: String,
-        default: 'uploads/default.jpg'
-    }
+const mongoose = require("mongoose");
+const db = require("../config/db");
+const bcrypt = require("bcrypt");
+const { Schema } = mongoose;
+const userScheme = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: "uploads/default.jpg",
+  },
 });
 
 //hash password
-userScheme.pre('save',async function(){
-    try{
-        var user=this;
-        const salt=await(bcrypt.genSalt(10));
-        const hashPass=await bcrypt.hash(user.password,salt);
-        user.password=hashPass;
-    }catch(e){
-        throw(e);
-    }
-})
+userScheme.pre("save", async function () {
+  try {
+    var user = this;
+    const salt = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(user.password, salt);
+    user.password = hashPass;
+  } catch (e) {
+    throw e;
+  }
+});
 // Kiểm tra email tồn tại
 userScheme.statics.isEmailExist = async function (email) {
-    const user = await this.findOne({ email });
-    return user !== null;
+  const user = await this.findOne({ email });
+  return user !== null;
 };
 
-const UserModel=db.model('user',userScheme);
-module.exports=UserModel;
+const UserModel = db.model("user", userScheme);
+module.exports = UserModel;
